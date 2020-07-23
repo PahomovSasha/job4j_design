b14aeae06bcd297ecdab201a446a80ddfa8d50f8
Sasha <caxap13@mail.ru>
2020-07-23 20:31:45 +0500

    5. Очередь на двух стеках [#309644] Поправлен метод poll

--- chapter_001/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -3,24 +3,16 @@ package ru.job4j.list;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int size = 0;
 
     public T poll() {
-        T elementIn = in.pop();
-        for (int i = 0; i < size - 1; i++) {
-            out.push(elementIn);
-            elementIn = in.pop();
+        int sizeIn = in.getSize();
+        for (int i = 0; i < sizeIn; i++) {
+            out.push(in.pop());
         }
-        size--;
-        for (int i = 0; i < size; i++) {
-            T elementOut = out.pop();
-            in.push(elementOut);
-        }
-        return elementIn;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        size++;
     }
 }
--- chapter_001/src/main/java/ru/job4j/list/SimpleStack.java
+++ chapter_001/src/main/java/ru/job4j/list/SimpleStack.java
@@ -3,16 +3,23 @@ package ru.job4j.list;
 import java.util.NoSuchElementException;
 
 public class SimpleStack<T> {
-    private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size = 0;
 
     public T pop() {
         if (linked.getSize() == 0) {
             throw new NoSuchElementException();
         }
+        size--;
         return linked.deleteLast();
     }
 
     public void push(T value) {
         linked.add(value);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
