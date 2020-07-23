8fa3127c5e7cdbe5151921a7ba289e19944d4681
Sasha <caxap13@mail.ru>
2020-07-23 21:40:08 +0500

    Реализовать коллекцию Set на массиве [#309667]

---
+++ chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,25 @@
+package ru.job4j.set;
+
+import ru.job4j.list.SimpleArray;
+
+import java.util.Iterator;
+
+public class SimpleSet<E> implements Iterable<E> {
+    private SimpleArray<E> simpleArray = new SimpleArray<>();
+    private int size = 0;
+
+    public void add(E e) {
+        for (int i = 0; i < size; i++) {
+            if (simpleArray.get(i) == e) {
+                return;
+            }
+        }
+        simpleArray.add(e);
+        size++;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return simpleArray.iterator();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleSetTest {
+
+    @Test
+    public void whenAddThenIt() {
+        SimpleSet<Integer> simpleSet = new SimpleSet<>();
+        simpleSet.add(1);
+        int rsl = simpleSet.iterator().next();
+        assertThat(rsl, is(1));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenAddDuplicateThenIt() {
+        SimpleSet<Integer> simpleSet = new SimpleSet<>();
+        simpleSet.add(1);
+        simpleSet.add(1);
+        Iterator<Integer> it = simpleSet.iterator();
+        it.next();
+        it.next();
+    }
+
+    @Test
+    public void whenSomeAddThenIt() {
+        SimpleSet<Integer> simpleSet = new SimpleSet<>();
+        simpleSet.add(1);
+        simpleSet.add(2);
+        simpleSet.add(3);
+        Iterator<Integer> it = simpleSet.iterator();
+        it.next();
+        it.next();
+        assertThat(it.next(), is(3));
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void whenMoreAddThenIt() {
+        SimpleSet<Integer> simpleSet = new SimpleSet<>();
+        simpleSet.add(1);
+        simpleSet.add(2);
+        Iterator<Integer> it = simpleSet.iterator();
+        it.next();
+        it.next();
+        it.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        SimpleSet<Integer> simpleSet = new SimpleSet<>();
+        simpleSet.iterator().next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        SimpleSet<Integer> simpleSet = new SimpleSet<>();
+        simpleSet.add(1);
+        Iterator<Integer> it = simpleSet.iterator();
+        simpleSet.add(2);
+        it.next();
+    }
+}
