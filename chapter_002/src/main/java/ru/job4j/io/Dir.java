c4ba63357a3878a1b30cbf83ae23e6cf500703ca
Sasha <caxap13@mail.ru>
2020-09-16 22:23:51 +0500

    4.0. File

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\projects\\job4j_design\\chapter_002\\src\\main\\java\\ru\\job4j\\io");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName() + ", " + String.format("size file: %s", subfile.length()) + " byte");
+        }
+    }
+}
