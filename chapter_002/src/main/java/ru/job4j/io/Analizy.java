00dbcbbeb51088051ab5eb82f591a688511a0d84
Sasha <caxap13@mail.ru>
2020-09-18 23:10:52 +0500

    2. Анализ доступности сервера. Вынес сохранение данных в файл в отдельный метод

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -1,21 +1,39 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizy {
 
+    /**
+     * Метод сохраняет в файл диапазоны времени недоступности сервера
+     * @param source исходный файл
+     * @param target выходной файл
+     */
     public void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             String serverDownTime = null;
             while (in.ready()) {
                 String line = in.readLine();
                 if ((line.contains("500 ") || line.contains("400 ")) && serverDownTime == null) {
                     serverDownTime = line.substring(4);
                 } else if ((line.contains("200 ") || line.contains("300 ")) && serverDownTime != null) {
-                    out.write(serverDownTime + " - " + line.substring(4) + ";");
+                    result.add(serverDownTime + " - " + line.substring(4) + ";");
                     serverDownTime = null;
                 }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        saveToFile(result, target);
+    }
+
+    private static void saveToFile(List<String> log, String target) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (String s : log) {
+                out.write(s + System.lineSeparator());
             }
         } catch (IOException e) {
             e.printStackTrace();
